2846a4dd96e15fc94886c12d84331732003eff1b
huwngnosleep <thanhhung8210@gmail.com>
2022-04-11 14:39:20 +0700

    redundant

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export { read, readFile } from "./codecs";
 export { write_str as to_text } from "./codecs/TXT";
 export * from "./types";
 
-const {readFile} = require('./codecs')
-readFile('test.docx')
